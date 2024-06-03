0991500bd06c8e730ebee9d38a8206e619738379
佳伟 <s2265681@163.com>
2024-06-03 14:37:22 +0800

    Initial Commit

---
+++ kalodata-production/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kalodata-production
+//
+//  Created by mac on 2024/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kalodata-production/kalodata_productionApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalodata_productionApp.swift
+//  kalodata-production
+//
+//  Created by mac on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct kalodata_productionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
